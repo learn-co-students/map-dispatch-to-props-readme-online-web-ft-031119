2ee3a716e8c330f28bbb7974734f42c1e66fed34
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-08-04 23:37:05 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   }
 
   render() {
+     
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -21,10 +22,6 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
 
-export default connect(mapStateToProps)(App);
+
+export default connect(state => ({ items: state.items }), { addItem })(App);
